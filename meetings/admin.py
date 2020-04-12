7f39c2f596aa978f83806d82835cd8431378ce6b
Sajal <raisulislam541@gmail.com>
2020-04-12 21:27:05 +0600

    [add][meeting planner]

---
+++ meetings/admin.py
@@ -0,0 +1,7 @@
+from .models import Meeting, Room
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Meeting)
+admin.site.register(Room)
+
---
+++ website/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from datetime import datetime, timedelta
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def welcome(request):
+    return render(request, "website/welcome.html")
+
+
+def date(request):
+    return HttpResponse(f'the page was server at {datetime.now() + timedelta(hours=6)}')
+
+
+def about(request):
+    return HttpResponse('i am raisul islam sajal and i am a junior software developer')
