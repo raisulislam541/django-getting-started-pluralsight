ec9f49453a28f06e0c67693b657e9f765f54dedf
Sajal <raisulislam541@gmail.com>
2020-04-14 06:05:57 +0600

    [add][urls of my apps to projects for best practice]

--- meeting_planner/urls.py
+++ meeting_planner/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from website.views import welcome, date, about
 from meetings.views import details, room_details
 
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', welcome, name='welcome'),
     path('date', date),
     path('about', about),
-    path('meetings/<int:id>', details, name="details"),
-    path('room', room_details, name='room')
+    path('meetings/', include('meetings.urls')),
 ]
+
+
---
+++ meetings/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:id>', views.details, name='details'),
+    path('room', views.room_details, name='room')
+]
